4e2a14ac70a62c53ad351bb8d00632987d513367
Abdul <lhlrahman@gmail.com>
2024-06-28 00:50:55 -0400

    Update src/maze_solver.h

--- src/maze_solver.h
+++ src/maze_solver.h
@@ -1,58 +1,27 @@
+// maze_solver.h
 #ifndef MAZE_SOLVER_H
 #define MAZE_SOLVER_H
 
 #include <vector>
 #include <string>
-#include <iostream>
-#include <stdexcept>
+#include "maze.h"
+#include "square.h"
 
-struct Square {
-    int row;
-    int column;
-    int index;
-    int border;
-    int role;
+std::vector<Square> bfs(const Maze &maze, const Square &start, const Square &goal, std::vector<std::vector<Square>>& steps);
+std::vector<Square> dfs(const Maze &maze, const Square &start, const Square &goal, std::vector<std::vector<Square>>& steps);
+std::vector<Square> dijkstra(const Maze &maze, const Square &start, const Square &goal, std::vector<std::vector<Square>>& steps);
+std::vector<Square> greedy_best_first(const Maze &maze, const Square &start, const Square &goal, std::vector<std::vector<Square>>& steps);
+std::vector<Square> wall_follower(const Maze &maze, const Square &start, const Square &goal, std::vector<std::vector<Square>>& steps);
+std::vector<Square> dead_end_filling(const Maze &maze, const Square &start, const Square &goal, std::vector<std::vector<Square>>& steps);
+std::vector<Square> recursive_backtracking(const Maze &maze, const Square &start, const Square &goal, std::vector<std::vector<Square>>& steps);
 
-    bool operator<(const Square& other) const {
-        return std::tie(row, column, index) < std::tie(other.row, other.column, other.index);
-    }
+void generate_html(const std::vector<Square>& path, const std::string& filename);
+void generate_html_animation(const std::vector<std::vector<Square>>& steps, const Maze& maze, const std::string& output_file, float delay, bool top_down);
 
-    bool operator==(const Square& other) const {
-        return row == other.row && column == other.column && index == other.index;
-    }
-};
-
-class Maze {
-public:
-    int width;
-    int height;
-    std::vector<Square> squares;
-
-    Maze(int w, int h, std::vector<Square> sq) : width(w), height(h), squares(sq) {}
-
-    Square get_square(int row, int column) const {
-        if (row < 0 || row >= height || column < 0 || column >= width) {
-            std::cerr << "Index out of bounds: row=" << row << ", column=" << column << std::endl;
-            throw std::out_of_range("Index out of bounds in get_square");
-        }
-        return squares.at(row * width + column);  // Use .at() for bounds checking
-    }
-
-    std::vector<Square> get_neighbors(const Square& square) const {
-        std::vector<Square> neighbors;
-        if (square.row > 0) neighbors.push_back(get_square(square.row - 1, square.column));
-        if (square.row < height - 1) neighbors.push_back(get_square(square.row + 1, square.column));
-        if (square.column > 0) neighbors.push_back(get_square(square.row, square.column - 1));
-        if (square.column < width - 1) neighbors.push_back(get_square(square.row, square.column + 1));
-        return neighbors;
-    }
-};
-
-// C-compatible function signatures
 extern "C" {
     void solve_maze_c(int width, int height, Square* squares, int start_row, int start_col, int goal_row, int goal_col, const char* algorithm, bool animation, float delay, const char* direction);
-    void generate_svg_c(const Square* path, int path_size, const char* output_file);
-    void generate_animation_c(const Square* steps, int* steps_sizes, int steps_count, const char* output_dir, float delay, bool top_down);
+    void generate_html_c(const Square* path, int path_length, const char* output_file);
+    void generate_html_animation_c(int width, int height, const Square* steps, int* step_lengths, int num_steps, const char* output_dir, float delay, bool top_down);
 }
 
 #endif // MAZE_SOLVER_H
